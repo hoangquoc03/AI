1bb249af3eb2d39e91a308fd7bb77932dbd484f6
lequoc <lequoc150305@quoc150305gmail.com>
2025-12-17 20:40:36 +0700

    ww

--- src/State/Store.ts
+++ src/State/Store.ts
@@ -1,6 +1,20 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-const rootReducer = combineReducers({});
-const store = configureStore({
-  reducer: rootReducer,
-  middleware:( getDefaultMiddleware) +>getDefaultMiddleware().concat(thunk)
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+const rootReducer = combineReducers({
+  // thêm reducer vào đây
 });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  // ❌ KHÔNG cần redux-thunk
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+
+// ===== Typed Hooks =====
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
+import { Provider } from "react-redux";
+import store from "./State/Store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
